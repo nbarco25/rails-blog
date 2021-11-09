d246ef3534ad87311e94232b22d0543e136a2303
nbarco25 <anbarco@unicauca.edu.co>
2021-11-08 20:34:01 -0500

    Add styles to index and articles form

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,12 +2,24 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap-sprockets
+//= require turbolinks
+//= require_tree .
+import 'bootstrap'
+import "../stylesheets/application"
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+require('packs/custom')
+require('packs/posts.js')
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+document.addEventListener("turbolinks:load", () => {
+  $('[data-toggle="tooltip"]').tooltip()
+  $('[data-toggle="popover"]').popover()
+})
