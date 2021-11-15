52df4f1ed5822ff4ce8f5282fa20d8d9363084d1
nbarco25 <anbarco@unicauca.edu.co>
2021-11-15 12:39:23 -0500

    Changes after feedback

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,16 +3,23 @@ class RelationshipsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-      blogger = User.find(params[:user_id])
-      @rel = Relationship.new(follower_id: current_user.id,
-                              followed_id: blogger.id)
-      @rel.save
+    blogger = User.find(params[:user_id])
+    @rel = Relationship.new(
+      follower_id: current_user.id,
+      followed_id: blogger.id)
+    if @rel.save
       redirect_to user_path(blogger)
+    else
+      redirect_to root_path
+    end
   end
 
   def destroy
-      @rel = Relationship.find(params[:id])
-      @rel.destroy
+    @rel = Relationship.find(params[:id])
+    if @rel.destroy
       redirect_to user_path(@rel.followed_id)
+    else
+      redirect_to root_path
+    end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   def show
-      @user = User.find(params[:id])
-      @rel = @user.followers.find_by(follower: current_user)
+    @user = User.find(params[:id])
+    @rel = @user.followers.find_by(follower: current_user)
   end
 end
