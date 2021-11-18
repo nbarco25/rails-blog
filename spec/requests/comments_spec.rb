2033c60bcca00b56ea5354ebb90800e7832bf0fd
nbarco25 <anbarco@unicauca.edu.co>
2021-11-18 09:27:25 -0500

    Add comments controller test

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -1,46 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe 'Comments', type: :controller do
-  let(:user) do
+  let(:current_user) do
     User.create(
       email: 'ang@hotmail.com',
-      password: 'qwerty12345',
+      password: 'qwerty12345'
     )
   end
 
   before do
-    sign_in user
+    sign_in current_user
   end
 
-  let(:article) do
-    Article.create(
-      title: 'Machine learning',
-      body: 'Machine learning is a interesting ...',
+  let(:valid_article_params) do
+    {
+      title: 'Hello',
+      body: 'This is a first article',
       status: 'public'
-    )
+    }
   end
 
-  let(:valid_params) do
+  let(:valid_comment_params) do
     {
       body: 'Cooool, great article',
-      status: 'public',
+      status: 'public'
     }
   end
 
   describe 'POST #create' do
-    context 'valid params' do
+    context 'valid params and authenticated user' do
       it 'creates a new comment' do
-        comment_params = {
-          comment: {
-            body: valid_params[:body],
-            status: valid_params[:status],
-          }
-      }
-
+        article = current_user.articles.create(valid_article_params)
+        article.save
+        comment = article.comments.create(valid_comment_params)
+        comment.id = '1'
+        comment.user_id = current_user.id
+        comment.commenter = current_user.email
+        expect(response).to be_successful
       end
     end
-
   end
-
-
 end
